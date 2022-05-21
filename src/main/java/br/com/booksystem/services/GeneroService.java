20bf3dc6919e234c3302210b85aa1ded59c02f0b
Tamires <tamiressiqueira12@gmail.com>
2022-05-21 18:43:36 -0300

    Adicionando logs a aplicação

--- src/main/java/br/com/booksystem/services/GeneroService.java
+++ src/main/java/br/com/booksystem/services/GeneroService.java
@@ -1,12 +1,28 @@
 package br.com.booksystem.services;
 
+import br.com.booksystem.model.Genero;
 import br.com.booksystem.repositories.GeneroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class GeneroService {
-	@Autowired
-	private GeneroRepository repository;
+    @Autowired
+    private GeneroRepository repository;
 
+    public boolean jaExiste(String nome) {
+        String nomeGenero = repository.findByNome(nome).get().getNome();
+        return nomeGenero.isEmpty() ? true : false;
+    }
+
+    public Genero findByNome(String nome) {
+        Optional<Genero> genero = repository.findByNome(nome);
+
+        return genero.get();
+    }
+    public void save(Genero genero){
+        repository.save(genero);
+    }
 }
--- src/main/java/br/com/booksystem/services/LivroService.java
+++ src/main/java/br/com/booksystem/services/LivroService.java
@@ -7,47 +7,55 @@ import br.com.booksystem.model.dto.LivroDTO;
 import br.com.booksystem.repositories.GeneroRepository;
 import br.com.booksystem.repositories.LivroRepository;
 import br.com.booksystem.repositories.TrechoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class LivroService {
-	@Autowired
-	private LivroRepository repository;
-	@Autowired
-	private TrechoRepository trechoRepository;
-	@Autowired
-	private GeneroRepository generoRepository;
-	public List<Livro> findAll() {
-		return repository.findAll();
-	}
+    @Autowired
+    private LivroRepository repository;
+    @Autowired
+    private TrechoService trechoService;
+    @Autowired
+    private GeneroService generoService;
 
-	public Livro save(Livro livro) {
-		for(Trecho t:livro.getTrechos()){
-			t.setLivro(livro);
-			trechoRepository.save(t);
-		}
-		for(Genero genero:livro.getGeneros()){
-			System.out.println(genero);
-			//comparar se já existe genero e não salvar se existir
-if (generoRepository.findByNome(genero.getNome()).isPresent()){
-	System.out.println("repetindo genero ");
-}else{
-	generoRepository.save(genero);
-}
+    public List<Livro> findAll() {
+        return repository.findAll();
+    }
 
-		}
+    public Livro save(Livro livro) {
+        for (Trecho t : livro.getTrechos()) {
+            t.setLivro(livro);
+            log.info("Salvando Trecho");
+			trechoService.save(t);
 
-		return repository.save(livro);
-	}
+        }
+        for (Genero genero : livro.getGeneros()) {
 
-	public List<Livro> getLivrosGenero(Long id) {
-		return repository.findByGeneros(id);
-	}
+            //comparar se já existe genero e não salvar se existir
+            if (generoService.jaExiste(genero.getNome())) {
+                Genero g=generoService.findByNome(genero.getNome());
+                generoService.save(g);
+				log.warn("repetindo genero");
+            } else {
+                log.info("Salvando Genero");
+                generoService.save(genero);
+            }
 
-	public Livro convertDto(LivroDTO dto){
-		return new Livro(dto.getNome(),dto.getAutor(),dto.getUrlCapa());
-	}
+        }
+         log.info("Salvando Livro");
+        return repository.save(livro);
+    }
+
+    public List<Livro> getLivrosGenero(Long id) {
+        return repository.findByGeneros(id);
+    }
+
+    public Livro convertDto(LivroDTO dto) {
+        return new Livro(dto.getNome(), dto.getAutor(), dto.getUrlCapa());
+    }
 }
